5052d0ed013c5b902cb950773f1c0ac11526e68c
akshay <akshay.patel@moreyeahs.co>
2023-06-18 09:54:48 +0530

    chnages to be pushed

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ import AppThemeProvider from "common/theme";
 import GlobalStyle from "global-styles";
 import { Suspense } from "react";
 import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <AppThemeProvider>
     <GlobalStyle />
     <Suspense fallback={<MainPageLoader />}>
-
-        <App />
-      
+      <App />
     </Suspense>
   </AppThemeProvider>
 );
+serviceWorkerRegistration.register();
